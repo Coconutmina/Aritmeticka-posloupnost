192105675867f114ac7c0f7c20f392d1c4ff5cb2
matěj <matj@DESKTOP-1HI5AKA>
2022-01-30 17:39:25 +0100

    Přidejte soubory projektu.

---
+++ Aritmeticka posloupnost/Aritmeticka posloupnost.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+//Aritmetická posloupnost
+
+int main(void) {
+
+	int rozdil = 1, predesle = 13;
+
+	printf("Vlozte první cislo poslopnosti: ");
+
+	printf("\nZadejte rozdíl: ");
+
+	scanf("%d", &predesle);
+	
+	scanf("%d", &rozdil);
+
+	printf("\n");
+
+	for (int i = 1; i <= 10; i++) {
+		
+		printf("Cislo slozka: %d: %d\n", i, predesle);
+		predesle += rozdil;
+	}
+}
+//doufám že to mám správně, fungovalo mi to jen jednou pak už ne tak snad.
